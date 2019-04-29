66984f8b580318032dd6a71ac2876d631b8b11d2
isalevine <isaacdlevine@gmail.com>
2019-04-28 22:25:14 -0700

    now able to create and save new SearchPreference instances using form data from frontend

---
+++ app/controllers/api/v1/search_preferences_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::SearchPreferencesController < ApplicationController
+
+  def show
+    find_search_preference
+    render json: @search_preference
+  end
+
+  def create
+    @search_preference = SearchPreference.new(search_preference_params)
+    if @search_preference.save
+      render json: @search_preference
+    end
+  end
+
+  # def update
+  #   find_search_preference
+  #   if @search_preference.update(search_preference_params)
+  #     render json: @search_preference
+  #   end
+  # end
+
+  def destroy
+    find_search_preference
+    @search_preference.destroy
+  end
+
+
+  private
+
+
+  def find_search_preference
+    @search_preference = SearchPreference.find(params[:id])
+  end
+
+  def search_preference_params
+    params.permit(:stat_preference, :action_preference, :power_preference)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
 
+  # UNCOMMENT do/end nesting below to enable has_many/belongs_to relationships
+  # (also, don't forget to look into Serializer!)
+
   namespace :api do
     namespace :v1 do
 
-      resources :base_characters do
-        resources :search_preferences
-      end
+      resources :base_characters # do
+        resources :search_preferences, only: [:show, :create, :update, :destroy]
+      # end
 
-      resources :archetypes do
+      resources :archetypes # do
         resources :search_lists
-      end
+      # end
 
     end
   end
