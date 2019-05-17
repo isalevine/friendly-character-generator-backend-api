3a6d1dd245a9af3e71ce5a4e878b435a1c3b57bf
isalevine <isaacdlevine@gmail.com>
2019-05-16 23:57:35 -0700

    began drafting backstory snippets

---
+++ app/models/test-models/test-backstory-snippets.rb
@@ -0,0 +1,27 @@
+
+
+# QUESTIONS TO CONSIDER:
+# 1. organize each hash into general: and system_unique: keys? (to allow for half-orc specific stuff in Dnd, and Solar stuff in Exalted)
+# 2. does each snipped need to be a hash that also contains (filtered) tags for searching individually? 
+#     => probably more efficient to search + gather tags WHEN CREATED, instead of during calculations
+#     => also, this hash would probably need an attribute to store very_beginning/near_beginning/etc., instead of using them as keys
+
+# big sword knight
+{
+    very_beginning: ["Birthed by an ironworker in an imperial steelmill during crunch-week for greatsword production, they were born with the smell of smelting metal in their nose and a fine coating of it in their lungs."],
+    near_beginning: ["The first time they touched a sword was at two weeks old, and at two months they mastered using a knife at the kitchen table. At two years they were babysat by squires who allowed swords to be used as toys, and by age twelve they mastered the blade--and became knighted."],
+    middle: ["Part of their oath is to only wield swords taller than they are--and they're pretty tall! Like 6-foot-something, at least.", "A fine military career was in the making, but they set off in search of a fabled swordsmith, who had already died by the time they found the old man's mountain workshop."],
+    near_end: ["They now seek a new as-of-yet undiscovered swordsmith master, who will craft an unbreakable sword of unrivaled sharpness."],
+    very_end: ["Since they sleep (and shower) in their full armor, they tend to reek--remember to do laundry before any stealth missions!"]
+}
+
+
+# smooth-talking ninja
+
+
+
+# corn-god-worshipping wizard
+
+
+
+# the mime
