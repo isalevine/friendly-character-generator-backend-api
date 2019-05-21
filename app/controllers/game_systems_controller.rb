5adee5b727a06b3df53829c5626cf8b5539bdebc
isalevine <isaacdlevine@gmail.com>
2019-05-20 17:45:51 -0700

    archetypes retrieved from database now rendering as json

--- app/controllers/game_systems_controller.rb
+++ app/controllers/game_systems_controller.rb
@@ -1,2 +1,8 @@
 class GameSystemsController < ApplicationController
+
+    def index
+        @game_systems = GameSystem.all.to_json
+        render json: @game_systems
+      end
+
 end
--- app/serializers/search_preference_serializer.rb
+++ app/serializers/search_preference_serializer.rb
@@ -1,3 +1,3 @@
 class SearchPreferenceSerializer < ActiveModel::Serializer
-  attributes :id, :base_character_id, :stat_preference, :action_preference, :power_preference
+  attributes :id, :stat_preference, :action_preference, :power_preference
 end
