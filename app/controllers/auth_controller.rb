806f02366b5c6bb2d639c97fd735abf3f99d1deb
isalevine <isaacdlevine@gmail.com>
2019-05-29 15:58:27 -0700

    changed auth-controller params to password_digest

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,7 @@ class AuthController < ApplicationController
 
     def create
         @user = User.find_by(username: user_login_params[:username])
-        if @user && @user.authenticate(user_login_params[:password])
+        if @user && @user.authenticate(user_login_params[:password_digest])
             token = encode_token({user_id: @user.id})
             render json: { user: @user, jwt: token }, status: :accepted
         else
@@ -15,7 +15,7 @@ class AuthController < ApplicationController
     private
 
     def user_login_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password_digest)
     end
 
 end
