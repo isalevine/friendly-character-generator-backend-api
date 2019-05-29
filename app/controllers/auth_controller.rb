12285e13795e2941d2bd722e440e43d42bbf01bf
isalevine <isaacdlevine@gmail.com>
2019-05-29 16:00:02 -0700

    rolled back password_digest changes in controllers

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,7 @@ class AuthController < ApplicationController
 
     def create
         @user = User.find_by(username: user_login_params[:username])
-        if @user && @user.authenticate(user_login_params[:password_digest])
+        if @user && @user.authenticate(user_login_params[:password])
             token = encode_token({user_id: @user.id})
             render json: { user: @user, jwt: token }, status: :accepted
         else
@@ -15,7 +15,7 @@ class AuthController < ApplicationController
     private
 
     def user_login_params
-        params.require(:user).permit(:username, :password_digest)
+        params.require(:user).permit(:username, :password)
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password_digest, :name, :email, :bio)
+        params.require(:user).permit(:username, :password, :name, :email, :bio)
     end
         
 end
